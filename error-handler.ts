b4ad60c62115acf97f032de6b906c1e18f1e319e
Clierik <38905813+Clierik@users.noreply.github.com>
2021-06-28 11:08:45 +0300

    Create error-handler.ts

---
+++ error-handler.ts
@@ -0,0 +1,15 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+
+@Injectible() export class ErrorHandler implements HttpInterceptor {
+
+constructor() {}
+
+intercept( request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+    return next.hanble(request).pipe(catchError( error => {
+      return observableTrowError(error);
+    }
+  ))
+}
