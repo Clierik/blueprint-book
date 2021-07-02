e88e5d4970e478aa2e03a0358e359a2c876d2b54
Clierik <38905813+Clierik@users.noreply.github.com>
2021-07-02 12:32:00 +0300

    Update header-handler.ts

--- header-handler.ts
+++ header-handler.ts
@@ -4,15 +4,16 @@ import { Observable } from "rxjs";
 
 @Injectable() 
 export class HeaderHandler implements HttpInterceptor {
+  
   constructor() {}
   
   intercept( request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        'your headers here...'
-      }
+        'your headers here...':'your headers here...'
+      },
       setParams: {
-        'your parameters here'
+        'your parameters here...':'your parameters here...'
       }
     });
     return next.handle(request);
